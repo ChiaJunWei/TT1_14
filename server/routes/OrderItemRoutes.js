5d48483075f195609f0d23b29a2391b8adcc4a01
gemabelanes <e0310795@u.nus.edu>
2021-09-30 15:08:17 +0800

    added auth middleware

--- server/routes/OrderItemRoutes.js
+++ server/routes/OrderItemRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import auth from '../middleware/auth.js';
 import { createOrderItem, updateOrderItem, getOrderItems, deleteOrderItem} from '../controller/OrderItemController.js';
 
 
 const router = express.Router();
 
-router.post('/createOrderItem', createOrderItem);
-router.post('/updateOrderItem', updateOrderItem);
-router.get('/getOrderItems', getOrderItems);
-router.post('/deleteOrderItem', deleteOrderItem);
+router.post('/createOrderItem',auth, createOrderItem);
+router.post('/updateOrderItem',auth, updateOrderItem);
+router.get('/getOrderItems',auth, getOrderItems);
+router.post('/deleteOrderItem',auth, deleteOrderItem);
 
 
 export default router;
