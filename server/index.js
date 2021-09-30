a6e14747c1a8e86434bdce56a2f092d378bec182
Ying Ci <54506588+YingCi56@users.noreply.github.com>
2021-09-30 11:45:17 +0800

    Created posts.js
    for post route

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
 
 dotenv.config();
 const app = express();
 
 const CONNECTION_URL = 'mongodb+srv://azurelle-dashboard:123123123@cluster0.jgioi.mongodb.net/react-pratice1?retryWrites=true&w=majority'
 
+app.use('/posts', postRoutes);
 app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
