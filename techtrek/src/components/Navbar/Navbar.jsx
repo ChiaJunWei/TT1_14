9dd5d8527fb1db02e5447ec670f61b7beac498d5
gemabelanes <e0310795@u.nus.edu>
2021-09-30 15:43:23 +0800

    added login logout button

--- techtrek/src/components/Navbar/Navbar.jsx
+++ techtrek/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,36 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Button } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { LOGOUT } from '../../Redux/actionTypes.js'
 
 const PrimarySearchAppBar = ({ totalItems }) => {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
   const classes = useStyles();
   const location = useLocation();
 
+  const user = useSelector((state) => state.authData);
+  const history = useHistory();
+
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
   const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  const dispatch = useDispatch()
+  function handleLogInOut() {
+    if(user) {
+      dispatch({type: LOGOUT});
+    } else {
+      history.push('/login')
+    }
+  }
 
   const renderMobileMenu = (
     <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
@@ -37,7 +52,15 @@ const PrimarySearchAppBar = ({ totalItems }) => {
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
             <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
           </Typography>
+          {user && 
+            <Typography>
+              {`Welcome ${user.name}`}
+            </Typography>
+          }
           <div className={classes.grow} />
+          <Button onClick = {handleLogInOut} variant = 'contained'>
+            {user ? "LOGOUT" : "LOGIN"}
+          </Button>
           {location.pathname === '/' && (
           <div className={classes.button}>
             <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
