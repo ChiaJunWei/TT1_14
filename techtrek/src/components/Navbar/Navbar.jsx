9b05a349ed14b384b591ad1eb5ce87051cde52ca
ChiaJunWei <83580418+ChiaJunWei@users.noreply.github.com>
2021-09-30 15:09:45 +0800

    login

--- techtrek/src/components/Navbar/Navbar.jsx
+++ techtrek/src/components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
             <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
           </Typography>
-          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+          <Typography component={Link} to="/login" variant="h6" className={classes.title} color="inherit">
              Log in
           </Typography>
           <div className={classes.grow} />
