77439af307874f974cdcf060815c2c299ae3a821
gemabelanes <e0310795@u.nus.edu>
2021-09-30 11:28:50 +0800

    added server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+const CONNECTION_URL = 'mongodb+srv://azurelle-dashboard:123123123@cluster0.jgioi.mongodb.net/react-pratice1?retryWrites=true&w=majority'
+
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+.catch((error) => console.log(error.message));
